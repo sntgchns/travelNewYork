5de299ec7a5566ac098a9f1386f02a4b304ed4f9
Florencia Paula Martinez <89614423+florenciapaula@users.noreply.github.com>
2022-01-22 13:17:49 -0300

    TemplateTravelNewYork
    v1

---
+++ javascript.js
@@ -0,0 +1,37 @@
+
+ function validacion(mail) {
+
+
+
+	var nombre = document.getElementById('nombre').value;
+	var mail = document.getElementById('mail').value;
+	var tel = document.getElementById('tel').value;
+	var subject = document.getElementById('subject').value;
+	var comentarios = document.getElementById('comentarios').value;
+
+
+	re=/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+
+	if(nombre != "" && mail != "" && tel != "" && subject != "" && comentarios!= ""){
+
+	if(!re.exec(mail)){
+
+	  alert('ingrese un mail valido');
+      return (false);
+	  
+	}
+	
+	else{
+      
+	  alert('Se abrira una ventana para enviar tu mensaje');
+	  window.location.href ='mailto:travelnewyork@gmail.com?subject='
+                         + encodeURIComponent(subject)
+                         + "&body=" 
+                         + encodeURIComponent(
+					     "Nombre y Apellido: " + nombre + "\r" +
+						 "Mail: " + mail + "\r" +
+						 "Tel: " + tel + "\r" +
+						 "Comentarios: " + comentarios);
+		
+	}
+}}
